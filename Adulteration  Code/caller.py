d87557c2bbaa8869d7c80eb32c4e13e35e9fee16
harshp77 <harshpandey00701@gmail.com>
2023-10-18 00:39:07 +0530

    sdasd

--- Adulteration  Code/caller.py
+++ Adulteration  Code/caller.py
@@ -8,24 +8,24 @@ import pandas as pd
 import glob
 import os
 
-# Image.store_masked("Grade1")
-# Image.store_masked("Grade2")
-# # Image.store_masked("Grade3")
+Image.store_masked("Grade1")
+Image.store_masked("Grade2")
+# Image.store_masked("Grade3")
 
-# img = cv.imread("sa.png")
-# # img = cv.resize(img,(1366,720),interpolation=cv.INTER_AREA)
-# # cv.imshow("",img)
-# # cv.waitKey(0)
+img = cv.imread("sa.png")
+# img = cv.resize(img,(1366,720),interpolation=cv.INTER_AREA)
+# cv.imshow("",img)
+# cv.waitKey(0)
 
-# Image.csv_store("masked_1")
-# Image.csv_store("masked_2")
-# Image.csv_store("masked_3")
+Image.csv_store("masked_1")
+Image.csv_store("masked_2")
+Image.csv_store("masked_3")
 
 
-# files = os.path.join("", "*.csv")
-# files = glob.glob(files)
-# print("Resultant CSV after joining all CSV files at a particular location...");
-# df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-# df = df.drop(["Unnamed: 0"],axis=1)
-# df.to_csv("final.csv")
+files = os.path.join("", "*.csv")
+files = glob.glob(files)
+print("Resultant CSV after joining all CSV files at a particular location...");
+df = pd.concat(map(pd.read_csv, files), ignore_index=True)
+df = df.drop(["Unnamed: 0"],axis=1)
+df.to_csv("final.csv")
 
--- Adulteration  Code/userperdaal.py
+++ Adulteration  Code/userperdaal.py
@@ -4,7 +4,7 @@ import joblib
 from prepross import *
 
 img = cv.imread("ref\g1pure2.png")
-# model = joblib.load("Color_Model.joblib")
+model = joblib.load("Color_Model.joblib")
 
 
 # Image.user_extractor(img,model)
